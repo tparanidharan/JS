c30596c16bf67d87d91fbdbf20c53f46eb37b3f6
Paranidharan Thangavelu <tparanidharan@gmail.com>
2021-05-25 23:10:50 +0530

    new1

--- Demo.js
+++ Demo.js
@@ -2,3 +2,18 @@ var name='Parani'
 console.log(name);
 name=name+"dharan Thangavelu1";
 console.log(name);
+// Pick a string. Your string can have any number of characters.
+var my_string = "G";
+
+// Calculate the ASCII value of the first character, i.e. the character at the position 0.
+var ASCII_value = my_string.charCodeAt(0);
+
+// Let us print
+console.log(ASCII_value);
+if('001' ==true)
+{
+  console.log("Hi");
+}
+else {
+  console.log("Hi222");
+}
