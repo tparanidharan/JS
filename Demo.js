b787fe794cdbc191849695b82721db8a586adb63
Paranidharan Thangavelu <tparanidharan@gmail.com>
2021-05-28 21:34:37 +0530

    updated

--- Demo.js
+++ Demo.js
@@ -1,4 +1,31 @@
-var name='Parani'
-console.log(name);
-name=name+"dharan Thangavelu";
-console.log(name +"Mittu");
+var donuts = ["glazed", "strawberry frosted", "powdered", "Boston creme"];
+
+donuts.pop();
+donuts.pop();
+donuts.pop();
+donuts.push("maple walnut");
+console.log(donuts.pop());
+console.log(donuts.push("sprinkled"));
+console.log(donuts);
+
+
+const bicycle = {
+  color: 'blue',
+  type: 'mountain bike',
+  wheels: {
+    diameter: 18,
+    width: 8
+  }
+};
+
+console.log(bicycle['type']);
+
+function changeToEight(n) {
+  n = 8; // whatever n was, it is now 8... but only in this function!
+}
+
+var n = 7;
+
+changeToEight(n);
+
+console.log(n);
